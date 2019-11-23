551ce0bab20420c84c76009674c938adbc90c5ca
고유빈 <gjsxjzlffj96@gmail.com>
2019-11-23 19:01:38 +0900

    -상수 입력

---
+++ FDM.py
@@ -0,0 +1,53 @@
+# constant
+import math
+
+Fuel_G1_Total = 4.682*math.exp(-1)
+Fuel_G1_Abs = 4.398*math.exp(-2)
+Fuel_G1_Scat_i_i = 4.232*math.exp(-1)
+Fuel_G1_Scat_i_j = 1.098*math.exp(-3)
+Fuel_G1_Nu = 3.768*math.exp(-2)
+Fuel_G2_Total = 1.025*math.exp(-0)
+Fuel_G2_Abs = 6.000*math.exp(-1)
+Fuel_G2_Scat_i_i = 4.258*math.exp(-1)
+Fuel_G2_Nu = 1.158*math.exp(0)
+Be_G1_Total = 5.164*math.exp(0)
+Be_G1_Abs = 6.623*math.exp(-3)
+Be_G1_Scat_i_i = 5.110*math.exp(0)
+Be_G1_Scat_i_j = 26.358*math.exp(-2)
+Be_G2_Total = 6.401*math.exp(0)
+Be_G2_Abs = 6.623*math.exp(-3)
+Be_G2_Scat_i_i = 6.392*math.exp(0)
+# h값 설정
+h = input("자를 간격을 입력하세요 : ")
+# Fast_group_Flux
+
+# FG_Left_Boundary
+2
+# FG_In_UO2
+
+# FG_Boundary_Between_UO2_and_Be
+
+# FG_In_Be
+
+# FG_Right_boundary
+
+# Thermal_group_Flux
+
+# TG_Left_Boundary
+
+# TFG_In_UO2
+
+# TG_Boundary_Between_UO2_and_Be
+
+# TG_In_Be
+
+# TG_Right_boundary
+
+# TDM
+
+# Keff
+
+#
+
+
+
